d364de680443a1072a192489366c9fe68a4b7b3d
Shaun Lusk <shaun.lusk@gmail.com>
2020-07-28 22:20:11 -0500

    React app test

---
+++ react-check/precache-manifest.1cc1011866a56e4fbd91cc595fa61c8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097f74628db34370b83dedbf3a73e75f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78be646afe8d0a0093bc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "31a6bdea2e9f05974dd1",
+    "url": "/static/js/2.13da6dee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.13da6dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78be646afe8d0a0093bc",
+    "url": "/static/js/main.f86d2bbf.chunk.js"
+  },
+  {
+    "revision": "fce86b0b0d3bb5c36281",
+    "url": "/static/js/runtime-main.7e7936e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
